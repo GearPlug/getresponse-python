9e4ca4daf6a5995e8e82fb4751f3603853b076f2
Miguel Ferrer <ingferrermiguel@gmail.com>
2018-02-26 08:59:46 -0500

    Add LICENSE and MANIFEST.In; Bump ver. 0.1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 
 setup(name='getresponse-python',
-      version='0.1',
+      version='0.1.1',
       description='Email Marketing Software',
       long_description=read('README.md'),
       url='https://github.com/GearPlug/getresponse-python',
